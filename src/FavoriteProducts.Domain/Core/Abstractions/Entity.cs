42585d21f51747c41f6da616f7561c5f819b296f
Pedro Netto <pedronetto31415@gmail.com>
2024-07-30 08:08:05 -0300

    feat: add entity abstraction

---
+++ src/FavoriteProducts.Domain/Core/Abstractions/Entity.cs
@@ -0,0 +1,28 @@
+﻿namespace FavoriteProducts.Domain.Core.Abstractions;
+
+public interface IEntity
+{
+    Guid Id { get; }
+}
+
+public abstract class Entity :
+    IEntity,
+    IEquatable<Entity>
+{
+    public Guid Id { get; } = Guid.NewGuid();
+
+    public bool Equals(Entity? other) =>
+        other is not null && Id.Equals(other.Id);
+
+    public sealed override bool Equals(object? obj) =>
+        obj is Entity other && Equals(other);
+
+    public sealed override int GetHashCode() =>
+        Id.GetHashCode();
+
+    public static bool operator ==(Entity? left, Entity? right) =>
+        left?.Equals(right) ?? right is null;
+
+    public static bool operator !=(Entity? left, Entity? right) =>
+        !(left == right);
+}
