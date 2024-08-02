0ed51e79a5d5434741337f7c1714a411c08e9e89
Pedro Netto <pedronetto31415@gmail.com>
2024-08-02 09:59:06 -0300

    feat(UseCases Layer): add validation pipeline

---
+++ src/FavoriteProducts.UseCases/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,43 @@
+using FavoriteProducts.UseCases.Abstractions;
+using FavoriteProducts.UseCases.Exceptions;
+using FluentValidation;
+using MediatR;
+using ValidationException = FavoriteProducts.UseCases.Exceptions.ValidationException;
+
+namespace FavoriteProducts.UseCases.Behaviors;
+
+internal sealed class ValidationBehavior<TCommand, TResponse>(
+    IEnumerable<IValidator<TCommand>> validators
+)
+    : IPipelineBehavior<TCommand, TResponse>
+    where TCommand : ICommandBase
+{
+    public async Task<TResponse> Handle(
+        TCommand request, 
+        RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if(!validators.Any())
+        {
+            return await next();
+        }
+
+        var context = new ValidationContext<TCommand>(request);
+
+        var validationErrors = await Task.WhenAll(
+            validators.Select(v => v.ValidateAsync(context, cancellationToken))
+        );
+
+        var failures = validationErrors
+            .SelectMany(result => result.Errors)
+            .ToList();
+
+        if(failures.Count != 0)
+        {
+            throw new ValidationException(
+                failures.Select(f => new ValidationError(f.PropertyName, f.ErrorMessage))
+            );
+        }
+
+        return await next();
+    }
+}
