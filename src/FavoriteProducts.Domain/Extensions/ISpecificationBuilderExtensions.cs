d04ce1ab91db322e4c7cbe97690894b933eb30e1
Pedro Netto <pedronetto31415@gmail.com>
2024-08-02 14:09:35 -0300

    refactor(Domain_Extensions): refactor ISpecificationBuilderExtensions.cs.SortBy to more cleanup form

--- src/FavoriteProducts.Domain/Extensions/ISpecificationBuilderExtensions.cs
+++ src/FavoriteProducts.Domain/Extensions/ISpecificationBuilderExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using Ardalis.Specification;
 using FavoriteProducts.Domain.Core.Abstractions;
@@ -8,34 +9,27 @@ namespace FavoriteProducts.Domain.Extensions;
 
 public static class ISpecificationBuilderExtensions
 {
-    public static ISpecificationBuilder<T> Paginate<T>(this ISpecificationBuilder<T> query, int page, int pageSize) =>
-        query
-            .Skip((page - 1) * pageSize)
-            .Take(pageSize);
-
-    public static IOrderedSpecificationBuilder<TEntity> SortBy<TEntity>(
-        this ISpecificationBuilder<TEntity> Query,
-        string sortBy, 
-        SortOrder sortOrder, 
-        IDictionary<string, Expression<Func<TEntity, object?>>> sortingExpresions) where TEntity : IEntity
+    public static ISpecificationBuilder<T> Paginate<T>(this ISpecificationBuilder<T> query, int page, int pageSize)
+    {
+        if (page <= 0 || pageSize <= 0)
         {
-            IOrderedSpecificationBuilder<TEntity>? orderedQuery = null;
-
-            if(!string.IsNullOrWhiteSpace(sortBy) && sortingExpresions.Keys.Contains(sortBy))
-            {
-                var sortExpression = sortingExpresions[sortBy]!;
-
-                if(sortOrder == SortOrder.Asc)
-                {
-                    orderedQuery = Query.OrderBy(sortExpression);
-                }
-                else
-                {
-                    orderedQuery = Query.OrderByDescending(sortExpression);
-                }
-            }
-
-            return orderedQuery ?? Query.OrderBy(p => p.Id);
+            throw new ArgumentException("Page and page size must be greater than zero.");
         }
 
+        return query
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize);
+    }
+
+    public static IOrderedSpecificationBuilder<TEntity> SortBy<TEntity>(
+        this ISpecificationBuilder<TEntity> query,
+        string sortBy, 
+        SortOrder sortOrder, 
+        IDictionary<string, Expression<Func<TEntity, object>>> sortingMap) where TEntity : IEntity =>
+        string.IsNullOrWhiteSpace(sortBy) ||
+        !sortingMap.TryGetValue(sortBy, out var sortExpression)
+            ? query.OrderBy(x => x.Id)
+            : sortOrder == SortOrder.Asc
+                ? query.OrderBy(sortExpression!)
+                : query.OrderByDescending(sortExpression!);
 }
