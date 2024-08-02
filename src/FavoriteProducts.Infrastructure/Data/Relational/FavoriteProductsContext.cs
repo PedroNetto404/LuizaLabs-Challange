74f590e44789e91b0d0de577c9cf0d96b7441fef
Pedro Netto <pedronetto31415@gmail.com>
2024-08-02 10:03:42 -0300

    feat(Infrastructure Layer): add db context

---
+++ src/FavoriteProducts.Infrastructure/Data/Relational/FavoriteProductsContext.cs
@@ -0,0 +1,25 @@
+﻿using FavoriteProducts.Domain.Core.Abstractions;
+using FavoriteProducts.Domain.Resources.Customers;
+using FavoriteProducts.Domain.Resources.FavoriteProducts;
+using FavoriteProducts.Domain.Resources.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace FavoriteProducts.Infrastructure.Data.Relational;
+
+public sealed class FavoriteProductsContext(
+    DbContextOptions<FavoriteProductsContext> options
+) : DbContext(options), IUnitOfWork
+{
+    public DbSet<FavoriteProduct> FavoriteProducts { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    
+    public async Task<bool> CommitAsync(CancellationToken cancellationToken = default) => 
+        await SaveChangesAsync(cancellationToken) > 0;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(FavoriteProductsContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
