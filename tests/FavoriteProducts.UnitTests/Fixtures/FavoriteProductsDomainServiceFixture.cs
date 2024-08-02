31f069477457f5018394b01fe62aa02aac2f661f
Pedro Netto <pedronetto31415@gmail.com>
2024-08-02 14:27:02 -0300

    feat(tests): add fixtures

---
+++ tests/FavoriteProducts.UnitTests/Fixtures/FavoriteProductsDomainServiceFixture.cs
@@ -0,0 +1,30 @@
+using FavoriteProducts.Domain.Core.Abstractions;
+using FavoriteProducts.Domain.Resources.AcrossAggregateServices;
+using FavoriteProducts.Domain.Resources.Customers;
+using FavoriteProducts.Domain.Resources.FavoriteProducts;
+using FavoriteProducts.Domain.Resources.Products;
+using Moq;
+
+namespace FavoriteProducts.UnitTests.Fixtures;
+
+public class FavoriteProductsDomainServiceFixture
+{
+    public Mock<IRepository<Customer>> CustomerRepositoryMock { get; set; }
+    public Mock<IRepository<Product>> ProductRepositoryMock { get; set; }
+    public Mock<IRepository<FavoriteProduct>> FavoriteProductsRepositoryMock { get; set; }
+    public Mock<IUnitOfWork> UnitOfWorkMock { get; set; }
+    public FavoriteProductDomainService FavoriteProductDomainService { get; set; }
+
+    public FavoriteProductsDomainServiceFixture()
+    {
+        FavoriteProductsRepositoryMock = new Mock<IRepository<FavoriteProduct>>();
+        ProductRepositoryMock = new Mock<IRepository<Product>>();
+        CustomerRepositoryMock = new Mock<IRepository<Customer>>();
+        UnitOfWorkMock = new Mock<IUnitOfWork>();
+        FavoriteProductDomainService = new FavoriteProductDomainService(
+            FavoriteProductsRepositoryMock.Object,
+            ProductRepositoryMock.Object,
+            CustomerRepositoryMock.Object,
+            UnitOfWorkMock.Object);
+    }
+}
