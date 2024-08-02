3fd118416afb12ca6f72d65156b6acef8d7897b7
Pedro Netto <pedronetto31415@gmail.com>
2024-08-02 14:30:10 -0300

    docs e [CONTINUE]: add postman and other migrations

---
+++ src/FavoriteProducts.Infrastructure/Migrations/20240802171857_add-product-title-to-favorited-project.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FavoriteProducts.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addproducttitletofavoritedproject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "product_title",
+                table: "favorite_products",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "product_title",
+                table: "favorite_products");
+        }
+    }
+}
