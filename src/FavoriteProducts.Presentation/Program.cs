44fbb8829b51a5dca6fea33d426aaad6fa35d324
Pedro Netto <pedronetto31415@gmail.com>
2024-08-03 21:09:51 -0300

    feat(log): add serilog

--- src/FavoriteProducts.Presentation/Program.cs
+++ src/FavoriteProducts.Presentation/Program.cs
@@ -2,6 +2,9 @@ using FavoriteProducts.Domain.Extensions;
 using FavoriteProducts.Infrastructure.Extensions;
 using FavoriteProducts.Presentation.Extensions;
 using FavoriteProducts.UseCases.Extensions;
+using Serilog;
+
+SetupLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -14,9 +17,32 @@ var builder = WebApplication.CreateBuilder(args);
         .AddUseCasesServices();
 }
 
-await builder
-    .Build()
-    .UsePipeline()
+var app = builder.Build();
+
+app.UsePipeline();
+
+app
     .ApplyMigration()
-    .SeedDatabaseIfDevelopment()
-    .RunAsync();
+    .SeedDatabaseIfDevelopment();
+
+try
+{
+    Log.Information("Starting web host");
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+static void SetupLogger() => Log.Logger =
+    new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Minute)
+        .WriteTo.Seq("http://seq:5341")
+        .Enrich.FromLogContext()
+        .CreateLogger();
