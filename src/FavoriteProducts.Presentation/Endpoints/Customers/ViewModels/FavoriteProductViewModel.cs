4b0b3ab84362db406adbbd692313848d55447d74
Pedro Netto <pedronetto31415@gmail.com>
2024-08-03 21:12:30 -0300

    fix: add favorite product id to FavoriteProductViewModel.cs

--- src/FavoriteProducts.Presentation/Endpoints/Customers/ViewModels/FavoriteProductViewModel.cs
+++ src/FavoriteProducts.Presentation/Endpoints/Customers/ViewModels/FavoriteProductViewModel.cs
@@ -5,6 +5,7 @@ using FavoriteProducts.UseCases.Features.FavoriteProducts.Dtos;
 namespace FavoriteProducts.Presentation.Endpoints.Customers.ViewModels;
 
 public sealed record FavoriteProductViewModel(
+    Guid Id,
     Guid ProductId,
     Guid CustomerId,
     string ProductTitle,
@@ -12,6 +13,7 @@ public sealed record FavoriteProductViewModel(
 {
     public static FavoriteProductViewModel FromDto(FavoriteProductDto dto) =>
         new(
+            dto.Id,
             dto.ProductId,
             dto.CustomerId,
             dto.ProductTitle,
